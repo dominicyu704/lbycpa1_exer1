3aae660864cd18101fe907b60979e0340b0e3a2f
dominicyu704 <dominic_yu@dlsu.edu.ph>
2020-01-28 01:44:38 +0800

    First commit

--- 11842784_exer1.py
+++ 11842784_exer1.py
@@ -1,1 +1,10 @@
+##################################################
 
+# Author: Dominic P. Yu
+# Date: January 27, 2020
+# Course: LBYCPA1
+
+# Program Description
+""" <This program is designed to give the formula and solve for acceleration and potential energy>
+"""
+##################################################
